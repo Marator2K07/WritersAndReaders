7875e96bf18171588dcc2da0934f30ef263e9eeb
Marator2K207 <dmitriyshaaa@gmail.com>
2023-08-03 15:40:49 +0300

    Обновление реализации под изменившеяся условия. Теперь писатели связаны с основным текстовым полем.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,11 +11,20 @@ MainWindow::MainWindow(QWidget *parent)
     baseLayout = new QVBoxLayout(centralWidget);
     // инициализация остальных атрибутов класса
     book = new Book();
-    manager = new WritersManager(2, book, this);
-    field = new QTextEdit("OLOLO", this);
+    manager = new WritersManager(5, book, this); // первый передаваемый параметр - количество писателей
+    field = new QTextEdit(this);
     field->setReadOnly(true);
     field->setStyleSheet("QTextEdit {background-color : black;"
-                         "color : mediumspringgreen}");
+                         "color : white}");
+    // каждый писатель теперь может показывать какое слово он дописал в книгу
+    // через виджет текствого поля
+    foreach (Writer *writer, manager->getWriters()) {
+        connect(writer, SIGNAL(writeWord(QString)), field, SLOT(insertHtml(QString)));
+    }
+    // ставим курсор в конец виджета текста
+    QTextCursor cursor = field->textCursor();
+    cursor.movePosition(QTextCursor::End);
+    field->setTextCursor(cursor);
     // layout settings
     baseLayout->addWidget(field);
     baseLayout->addWidget(manager);
