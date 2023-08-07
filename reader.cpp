7c168b01fd5e41bc94e43f117f026f23e1edc4f3
Marator2K207 <dmitriyshaaa@gmail.com>
2023-08-07 12:17:31 +0300

    Теперь класс Reader в случае активности писателей - засыпает. Добавлены необходимые атрибуты для работы с приостановкой потока. Также как и у класса Writer создан универсальный атрибут для длительности паузы.

--- reader.cpp
+++ reader.cpp
@@ -1,8 +1,13 @@
 
 #include "reader.h"
 
-Reader::Reader(QObject *parent)
-    : QObject{parent}
+Reader::Reader(QWaitCondition *writersInactivity,
+               QMutex *waitConditionLocker,
+               QObject *parent)
+    : writersInactivity{writersInactivity}
+    , waitConditionLocker{waitConditionLocker}
+    , pauseDuration{700}
+    , QObject{parent}
 {
 }
 
@@ -11,34 +16,36 @@ void Reader::completingWork()
     /// тестовая реализация, близко к истине
 
     // подготовительная часть
-    short defaultPause = 1500;
-    short leftPauseBorder = 2222;
-    short rightPauseBorder = 3333;
-    emit updateInfo("Читатель пришел");
     emit updateBookText(&bookText);
-    QThread::msleep(QRandomGenerator::global()->bounded(leftPauseBorder, rightPauseBorder));
+    emit updateInfo("Читатель пришел");
+    QThread::msleep(QRandomGenerator::global()->bounded(pauseDuration, pauseDuration * 2));
     emit updateInfo("Начинает читать книгу");
-    QThread::msleep(defaultPause);
+    QThread::msleep(pauseDuration);
 
+    bool writersIsActive = false;
     foreach (QString line, bookText) {
-        // в случае прихода писателя
-        if (thread()->isInterruptionRequested()) {
-            emit updateInfo("Прекращает читать, так как пришел писатель");
-            QThread::msleep(defaultPause);
-            emit updateInfo("");
-            QThread::msleep(defaultPause);
-            emit endExecution(); // завершаем поток чтения одновременно с завершением метода
-            return;
+        emit checkWritersActivity(&writersIsActive);
+        // на каждой итерации смотрим, не появился ли писатель,
+        // если появился, то переходим в режим ожидания его ухода
+        if (writersIsActive) {
+            waitConditionLocker->lock();
+            emit updateInfo("Ждет, пока писатель/писатели допишет/допишут книгу");
+            QThread::msleep(pauseDuration / 2);
+            writersInactivity->wait(waitConditionLocker);
+            emit updateInfo("Продолжает читать после ухода писателя");
+            QThread::msleep(pauseDuration / 2);
+            waitConditionLocker->unlock();
         }
         emit updateInfo(line);
-        QThread::msleep(650);
+        QThread::msleep(pauseDuration / 2);
+
     }
 
     // заключительная часть
     emit updateInfo("Закончил читать");
-    QThread::msleep(defaultPause);
+    QThread::msleep(pauseDuration);
     emit updateInfo("Читатель ушел");
-    QThread::msleep(defaultPause);
+    QThread::msleep(pauseDuration);
     emit updateInfo("");
     emit endExecution(); // завершаем поток чтения одновременно с завершением метода
 }
--- reader.h
+++ reader.h
@@ -6,23 +6,32 @@
 #include <QRandomGenerator>
 #include <QList>
 #include <QThread>
+#include <QMutex>
+#include <QWaitCondition>
 
 class Reader : public QObject
 {
     Q_OBJECT
 private:
     QList<QString> bookText;
+    const short pauseDuration; // в миллисекундах
+    QWaitCondition *writersInactivity;
+    QMutex *waitConditionLocker;
 
 public:
-    explicit Reader(QObject *parent = nullptr);
+    explicit Reader(QWaitCondition *writersInactivity,
+                    QMutex *waitConditionLocker,
+                    QObject *parent = nullptr);
 
 signals:
+    /// таким образом узнаем, активен ли в данный момент какой либо из писателей
+    void checkWritersActivity(bool *activity);
     /// сигнал о необходимости подгрузки всего текста книги
     void updateBookText(QList<QString> *text);
     /// для оповещения происходящих событий в виджете надписи
     void updateInfo(const QString);
     /// уведомление завершения основного кода для выполнения
-    void endExecution();
+    void endExecution();    
 
 public slots:
     ///
