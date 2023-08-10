08d92c941a2d70d72e5a9666934a56645d012e2d
Marator2K207 <dmitriyshaaa@gmail.com>
2023-08-10 12:56:32 +0300

    Теперь подрузка и очистка текста книги идет по ходу выполнения приложения, а не подгружается все и один раз, как было раньше.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,6 +19,10 @@ MainWindow::MainWindow(QWidget *parent)
     field->setReadOnly(true);
     field->setStyleSheet("QTextEdit {background-color : black;"
                          "color : white}");
+    // очистка поля от лишнего текста ради оптимизации
+    connect(book, SIGNAL(clearText()), field, SLOT(clear()));
+    // подгрузка уже обновленного текста книги
+    connect(book, SIGNAL(updateText(QList<QString>)), field, SLOT(updateText(QList<QString>)));
     // каждый писатель теперь может показывать какое слово он дописал в книгу через виджет текствого поля
     foreach (Writer *writer, writersManager->getWriters()) {
         connect(writer, SIGNAL(writeWord(QString)), field, SLOT(insertHtmlWithAutoScroll(QString)));
@@ -26,11 +30,6 @@ MainWindow::MainWindow(QWidget *parent)
     // каждый читатель в состоянии узнать, активен ли хоть один писатель в данный момент времени
     foreach (Reader *reader, readersManager->getReaders()) {
         connect(reader, SIGNAL(checkWritersActivity(bool*)), writersManager, SLOT(writersActivityAnalysis(bool*)));
-    }
-    // заполняем поле текста книги
-    QList<QString> bookLines = book->getText();
-    foreach (QString line, bookLines) {
-        field->append(line);
     }
     // layout settings
     baseLayout->addWidget(field);
